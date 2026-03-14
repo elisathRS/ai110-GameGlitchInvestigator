262a92dd016c6fc19ab740c955d247a425a3d89f
elisathRS <mary0605@gmail.com>
2026-03-14 17:32:44 -0400

    Fix the tests

--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -1,16 +1,17 @@
-from logic_utils import check_guess
+from logic_utils import check_guess, get_range_for_difficulty
+
 
 def test_winning_guess():
     # If the secret is 50 and guess is 50, it should be a win
-    outcome = check_guess(50, 50)
+    outcome, message = check_guess(50, 50)
     assert outcome == "Win"
 
 def test_guess_too_high():
     # If secret is 50 and guess is 60, hint should be "Too High"
-    outcome= check_guess(60, 50)
+    outcome, message = check_guess(60, 50)
     assert outcome == "Too High"
 
 def test_guess_too_low():
     # If secret is 50 and guess is 40, hint should be "Too Low"
-    outcome = check_guess(40, 50)
+    outcome, message = check_guess(40, 50)
     assert outcome == "Too Low"
